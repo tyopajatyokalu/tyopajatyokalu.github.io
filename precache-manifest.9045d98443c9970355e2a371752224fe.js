525281d856b6e1e2510a2d3f7f2488493013c171
Martta Jämsén <martta.jamsen@gmail.com>
2019-06-17 15:18:01 +0300

    Updates

---
+++ precache-manifest.9045d98443c9970355e2a371752224fe.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "89648b4eef3e5997d4c0",
+    "url": "/static/css/main.288788c3.chunk.css"
+  },
+  {
+    "revision": "89648b4eef3e5997d4c0",
+    "url": "/static/js/main.6347f774.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "08c2d1ba253dd1564582",
+    "url": "/static/js/2.b5bd90d3.chunk.js"
+  },
+  {
+    "revision": "3be67a885536f16ad350af80d537b127",
+    "url": "/static/media/maisema_EevaHelle_IMG_9089.3be67a88.jpg"
+  },
+  {
+    "revision": "ef566c49dbc7a7f49fe14f069d5594fb",
+    "url": "/static/media/Tondu_Beta.ef566c49.ttf"
+  },
+  {
+    "revision": "3f482321becfaa40fcb19825fa717859",
+    "url": "/static/media/SourceSansPro-Regular.3f482321.ttf"
+  },
+  {
+    "revision": "7e0ee8c45d872446649aacd3ce96e1f7",
+    "url": "/static/media/SourceSansPro-Bold.7e0ee8c4.ttf"
+  },
+  {
+    "revision": "a0334b68343f36cbd271bfd26de969ef",
+    "url": "/index.html"
+  }
+];
